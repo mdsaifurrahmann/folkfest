7c5397e23c50ba68bb9af91474ee410d6dfdd9a0
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2025-03-04 03:40:28 +0600

    join us page marked as complete

---
+++ src/js/join-us.js
@@ -0,0 +1,27 @@
+import "../css/join-us.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let sffjoin = document.querySelector("#joinus");
+
+  if (sffjoin) {
+    const sffjoinslider = new Swiper(sffjoin, {
+      slidesPerView: 2,
+      spaceBetween: 8,
+      centeredSlides: true,
+      cssMode: true,
+      breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+          centeredSlides: false,
+          cssMode: true,
+        },
+      },
+    });
+  }
+});
