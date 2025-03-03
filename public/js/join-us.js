99cec5b7215e52fd63c562f1ed9517603f4dad41
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2025-03-04 03:50:33 +0600

    join us build version

---
+++ public/js/join-us.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector("#joinus");e&&new Swiper(e,{slidesPerView:2,spaceBetween:8,centeredSlides:!0,cssMode:!0,breakpoints:{576:{slidesPerView:2},768:{slidesPerView:3},1024:{slidesPerView:4,centeredSlides:!1,cssMode:!0}}})});
